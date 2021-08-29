97f0ccd1a11ab57c34b4ec144f9afb9c4641cb99
Frederic <paulpoiu123@gmail.com>
2021-08-29 23:56:16 +0200

    加入專案檔案。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Entity;
+
+
+namespace WPF_ADO_Learning
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NorthwindEntities context = new NorthwindEntities();
+        CollectionViewSource custViewSource;
+        CollectionViewSource ordViewSource;
+        public MainWindow()
+        {
+            InitializeComponent();
+            custViewSource = ((CollectionViewSource)(FindResource("customersViewSource")));
+            ordViewSource = ((CollectionViewSource)(FindResource("customersOrdersViewSource")));
+            DataContext = this;
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            //System.Windows.Data.CollectionViewSource customersViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("customersViewSource")));
+            // 透過設定 CollectionViewSource.Source 屬性載入資料: 
+            // customersViewSource.Source = [泛用資料來源]
+            // Load is an extension method on IQueryable,    
+            // defined in the System.Data.Entity namespace.   
+            // This method enumerates the results of the query,    
+            // similar to ToList but without creating a list.   
+            // When used with Linq to Entities, this method    
+            // creates entity objects and adds them to the context.   
+            context.Customers.Load();
+
+            // After the data is loaded, call the DbSet<T>.Local property    
+            // to use the DbSet<T> as a binding source.   
+            custViewSource.Source = context.Customers.Local;
+            //System.Windows.Data.CollectionViewSource customersViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("customersViewSource")));
+            // 透過設定 CollectionViewSource.Source 屬性載入資料: 
+            // customersViewSource.Source = [泛用資料來源]
+        }
+    }
+}
+
+
+//Reference
+//https://docs.microsoft.com/zh-tw/visualstudio/data-tools/create-a-simple-data-application-with-wpf-and-entity-framework-6?view=vs-2019
